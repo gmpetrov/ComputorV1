96429cfc0cfb86a89ef22df12bb2425bd61aa733
Georges-michael PETROV <gpetrov@e1r3p14.42.fr>
2015-06-04 16:07:57 +0200

    Computor v1 JS version

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"flag"
+	// "flag"
 	"fmt"
 	"regexp"
-	"strconv"
-	"strings"
+	// "strconv"
+	// "strings"
 )
 
 type operator string
@@ -33,36 +33,42 @@ func errorFormat() {
 	fmt.Println("Wrong input format")
 }
 
-func checkArgs() {
-	flag.Parse()
-	if flag.NArg() >= 1 {
-		eq := strings.Split(flag.Arg(0), " ")
-		fmt.Println(eq)
-		for k, v := range eq {
-			if v == MUL {
-				// THIS IS A TERM
-				if k-1 < 0 { // ERROR CASE WHERE THE EQUATION BEGIN BY A MUL OPERATOR
-					errorFormat()
-					return
-				}
-				_, err := strconv.Atoi(eq[k-1])
-				if err == nil {
-					fmt.Println("OK")
-				} else {
-					fmt.Println("Wrong input format")
-				}
-			}
+// func checkArgs() {
+// 	flag.Parse()
+// 	if flag.NArg() >= 1 {
+// 		eq := strings.Split(flag.Arg(0), " ")
+// 		fmt.Println(eq)
+// 		for k, v := range eq {
+// 			if v == MUL {
+// 				// THIS IS A TERM
+// 				if k-1 < 0 { // ERROR CASE WHERE THE EQUATION BEGIN BY A MUL OPERATOR
+// 					errorFormat()
+// 					return
+// 				}
+// 				_, err := strconv.Atoi(eq[k-1])
+// 				if err == nil {
+// 					fmt.Println("OK")
+// 				} else {
+// 					fmt.Println("Wrong input format")
+// 				}
+// 			}
 
-			num, err := strconv.Atoi(v)
-			if err == nil {
-				fmt.Println(num)
-			} else {
-				fmt.Println(err)
-			}
-		}
-	} else {
-		fmt.Println("No argument")
-	}
+// 			num, err := strconv.Atoi(v)
+// 			if err == nil {
+// 				fmt.Println(num)
+// 			} else {
+// 				fmt.Println(err)
+// 			}
+// 		}
+// 	} else {
+// 		fmt.Println("No argument")
+// 	}
+// }
+
+func checkArgs() {
+
+	r, _ := regexp.Compile("(.*) =")
+	String("5 * X^0 + 4 * X^1 - 9.3 * X^2 = 1 * X^0"))
 }
 
 func main() {
