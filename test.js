d339c9799ef6d753893e258226d491c657ed0ad0
Georges-michael PETROV <gpetrov@e1r3p14.42.fr>
2015-06-05 17:48:52 +0200

    Crazy Regexp for checking equation's validity

--- test.js
+++ test.js
@@ -1,13 +1,14 @@
 var regexp 			= require('./regexp').create;
 
-casper.test.begin("REGEXP TEST", 8, function(test) {
+casper.test.begin("REGEXP TEST", 9, function(test) {
 	test.assert(regexp.isNum.test("123456"), "isNUm : 123456");
 	test.assert(regexp.isNum.test("-123456"), "isNUm : -123456");
 	test.assertNot(regexp.isNum.test("123a56"), "isNum : 123a56");
 	test.assertEquals(regexp.getExposants.exec("X^-1")[1], "-1");
 	test.assertNot(regexp.isNum.test("123a56"), "isNum : 123a56");
+    test.assert(regexp.isEquationValid.test("5 * X^0 + 4 * X^1 - 9 * X^2 = 1 * X^2"));
     test.assertNot(regexp.isEquationValid.test("A * X^0 + 4 * X^1 - 9.3 * X^2 = 1 * X^2"));
     test.assertNot(regexp.isEquationValid.test("5 * X^0 + 4 * X^1 - 9.3 * X^2 = 1 * X^B"));
-    test.assertNot(regexp.isEquationValid.test("5 * X^0 + 4 * X^1 - 9.3 * X^2 1 * X^2"));
+    test.assert(regexp.isEquationValid.test("5 * X^0 + 4 * X^1 - 9.3 * X^2 = 1 * X^2"));
 	test.done();
 });
