2a70b77d4081c9d9b3edbd24b3c2d9a97935f932
Georges-michael PETROV <gpetrov@e1r3p14.42.fr>
2015-06-05 18:18:42 +0200

    Update Crazy Regexp

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 var regexp 			= require('./regexp').create;
 
-casper.test.begin("REGEXP TEST", 9, function(test) {
+casper.test.begin("REGEXP TEST", 11, function(test) {
 	test.assert(regexp.isNum.test("123456"), "isNUm : 123456");
 	test.assert(regexp.isNum.test("-123456"), "isNUm : -123456");
 	test.assertNot(regexp.isNum.test("123a56"), "isNum : 123a56");
@@ -10,5 +10,7 @@ casper.test.begin("REGEXP TEST", 9, function(test) {
     test.assertNot(regexp.isEquationValid.test("A * X^0 + 4 * X^1 - 9.3 * X^2 = 1 * X^2"));
     test.assertNot(regexp.isEquationValid.test("5 * X^0 + 4 * X^1 - 9.3 * X^2 = 1 * X^B"));
     test.assert(regexp.isEquationValid.test("5 * X^0 + 4 * X^1 - 9.3 * X^2 = 1 * X^2"));
+    test.assert(regexp.isEquationValid.test("5 *X^0 + 4 * X^1 - 9.3 * X^2 = 1 * X^2"));
+    test.assert(regexp.isEquationValid.test("5*X^0+4*X^1-9.3*X^2=1*X^2"));
 	test.done();
 });
